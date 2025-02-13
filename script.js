8a42d19fbb2b81701f10595a85d6cf354e368ac7
LoakaSsj <luisluna.lunaluis.lol@gmail.com>
2025-02-12 22:26:46 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const yesButton = document.getElementById('yes-button');
+const noButton = document.getElementById('no-button');
+
+noButton.addEventListener('click', () => {
+    noButton.style.fontSize = `${parseFloat(window.getComputedStyle(noButton).fontSize) * 0.8}px`;
+    noButton.style.position = 'absolute';
+    noButton.style.top = `${Math.random() * window.innerHeight - noButton.offsetHeight}px`;
+    noButton.style.left = `${Math.random() * window.innerWidth - noButton.offsetWidth}px`;
+    yesButton.style.fontSize = `${parseFloat(window.getComputedStyle(yesButton).fontSize) * 1.2}px`;
+});
