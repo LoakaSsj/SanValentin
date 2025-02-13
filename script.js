13e40f26fb6413db7bbd3d3851714a6b820dc8e4
LoakaSsj <luisluna.lunaluis.lol@gmail.com>
2025-02-12 22:39:32 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const yesButton = document.getElementById('yes-button');
 const noButton = document.getElementById('no-button');
 
 noButton.addEventListener('click', () => {
+    
     noButton.style.fontSize = `${parseFloat(window.getComputedStyle(noButton).fontSize) * 0.8}px`;
     noButton.style.position = 'absolute';
     noButton.style.top = `${Math.random() * window.innerHeight - noButton.offsetHeight}px`;
